784ecfa852e493ece40eacd28c9bd986802e3ff4
Nate Dietz <nsdietz@gmail.com>
2019-11-14 14:06:23 -0500

    seeded db

--- coffee-review-api-backend/db/seeds.rb
+++ coffee-review-api-backend/db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+dragonfly = Roaster.create(:name => "Dragonfly", :geo => "Colorado")
+espresso_lab = Roaster.create(:name => "The Espresso Lab", :geo => "Dubai")
+sightglass = Roaster.create(:name => "Sightglass", :geo => "San Francisco")
+
+Roast.create(
+  :name => "Creme Dolce Espresso",
+  :origin => "Columbia",
+  :roasted_date => Date.today,
+  :tasting_notes => "Caramel, Chocolate, Blueberry",
+  :preparation_method => "Pour Over",
+  :price => 20,
+  :roaster_id => dragonfly.id
+  )
+
+Roast.create(
+  :name => "Project Origin",
+  :origin => "Ethiopia",
+  :roasted_date => Date.today,
+  :tasting_notes => "Nectarine, Orange Blossom, Lime",
+  :preparation_method => "Moka Pot",
+  :price => 20,
+  :roaster_id => espresso_lab.id
+  )
+
+Roast.create(
+  :name => "Owl's Howl",
+  :origin => "Panama",
+  :roasted_date => Date.today,
+  :tasting_notes => "Dark Chocolate, Raspberry",
+  :preparation_method => "Espresso",
+  :price => 18,
+  :roaster_id => sightglass.id
+  )
