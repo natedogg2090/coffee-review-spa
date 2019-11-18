43621908fca55e32bdaada54274ea583c90109a6
Nate Dietz <nsdietz@gmail.com>
2019-11-18 12:53:01 -0500

    created render method to render roasters to the DOM in an ul

--- coffee-review-js-frontend/src/components/roaster.js
+++ coffee-review-js-frontend/src/components/roaster.js
@@ -7,9 +7,33 @@ class Roaster {
 	}
 
 	fetchAndLoadRoasters() {
-		this.adapter.getRoasters().then(roasters => {
-			console.log(roasters)
+		this.adapter
+		.getRoasters()
+		.then(roasters => {
+			roasters.forEach(roaster => this.roasters.push(roaster))
 		})
+		.then(() => {
+			this.renderRoasters()
+		})
+		
+	}
+
+	renderRoasters() {
+		const roasterContainer = document.getElementById('roasters-container')
+		const div = document.createElement('div')
+		const ul = document.createElement('ul')
+		roasterContainer.appendChild(div)
+		div.appendChild(ul)
+
+		console.log('here are my roasters', this.roasters)
+
+		this.roasters.forEach(roaster => {
+			let li = document.createElement('li')
+			li.setAttribute("class", `roaster-${roaster.id}`)
+			li.innerHTML = `${roaster.name}`
+			ul.appendChild(li)
+		})
+
 		
 	}
 }
